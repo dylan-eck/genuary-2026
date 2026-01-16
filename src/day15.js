7c470885bfbe6a24d12c6c0f721fa9e60d70e612
Dylan Eck <akdylaneck@gmail.com>
2026-01-15 21:46:17 -0600

    sand trails working

--- src/day15.js
+++ src/day15.js
@@ -1,13 +1,16 @@
 export default function day15(p) {
-  const ALPHA = 10;
   let res, numRows, numCols, grid;
   let rot = 0;
+  let trailLayer;
 
   p.setup = () => {
     p.createCanvas(1080, 1920);
     p.pixelDensity(1);
 
-    res = 4;
+    trailLayer = p.createGraphics(p.width, p.height);
+    trailLayer.noStroke();
+
+    res = 8;
     numRows = p.height / res;
     numCols = p.width / res;
     grid = Array(numRows * numCols).fill(0);
@@ -16,16 +19,14 @@ export default function day15(p) {
   };
 
   p.draw = () => {
-    p.background(0);
-    p.noStroke();
-
+    p.clear();
     p.push();
-    p.stroke("red");
-    p.strokeWeight(150);
+    p.stroke(255);
+    p.strokeWeight(180);
     p.strokeCap(p.SQUARE);
     p.noFill();
-    const nextGrid = Array(numRows * numCols).fill(0);
-    rot += 0.0001 * p.deltaTime;
+
+    rot += 0.0002 * p.deltaTime;
     const x = p.width / 2;
     const y = p.height / 2;
     const r = 400;
@@ -33,6 +34,9 @@ export default function day15(p) {
       const theta = (p.map(i, 0, 5, 0, p.TAU) + rot) % p.TAU;
       p.line(x, y, x + r * p.cos(theta), y + r * p.sin(theta));
     }
+    p.pop();
+
+    const nextGrid = Array(numRows * numCols).fill(0);
 
     p.loadPixels();
     for (let i = 0; i < grid.length; i++) {
@@ -47,10 +51,11 @@ export default function day15(p) {
         nextGrid[i] = null;
       }
     }
-    p.pop();
 
-    // SAND SIM =======
-    for (let i = 0; i < 4; i++) {
+    trailLayer.fill(0, 20);
+    trailLayer.rect(0, 0, p.width, p.height);
+
+    for (let i = 0; i < 8; i++) {
       grid[p.floor(p.random(numCols))] = 255;
     }
 
@@ -58,12 +63,11 @@ export default function day15(p) {
       const x = i % numCols;
       const y = p.floor(i / numCols);
 
-      if (nextGrid[i] === null) {
-        p.fill("red");
-        p.rect(x * res, y * res, res, res);
+      if (grid[i] === 0 || nextGrid[i] === null) continue;
+      if (grid[i] === null) {
+        nextGrid[i] === null;
         continue;
       }
-      if (grid[i] <= ALPHA) continue;
 
       const belowIdx = x + (y + 1) * numCols;
 
@@ -80,27 +84,22 @@ export default function day15(p) {
           return grid[idx] === 0 && nextGrid[idx] !== null;
         });
 
-        if (possibleIndices.includes(belowIdx)) {
-          nextGrid[belowIdx] = 255;
-        } else {
-          nextGrid[p.random(possibleIndices)] = 255;
+        if (possibleIndices.length > 0) {
+          if (possibleIndices.includes(belowIdx)) {
+            nextGrid[belowIdx] = 255;
+          } else {
+            const target = p.random(possibleIndices);
+            nextGrid[target] = 255;
+          }
         }
-
-        const belowLeftIdx = belowIdx - 1;
-        const belowRightIdx = belowIdx + 1;
       }
 
-      p.fill(grid[i]);
-      p.rect(x * res, y * res, res, res);
+      trailLayer.fill(255);
+      trailLayer.rect(x * res, y * res, res, res);
     }
     grid = [...nextGrid];
-  };
 
-  p.mouseDragged = () => {
-    const x = p.floor(p.mouseX / res);
-    const y = p.floor(p.mouseY / res);
-    const idx = x + y * numCols;
-
-    grid[idx] = 255;
+    p.background(0);
+    p.image(trailLayer, 0, 0);
   };
 }
