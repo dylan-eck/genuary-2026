69fd09d77e6fa153a1ada3278a1a6c6b28d792eb
Dylan Eck <akdylaneck@gmail.com>
2026-03-15 16:55:22 -0500

    fix svg canvas not being removed

--- src/30/index.js
+++ src/30/index.js
@@ -5,6 +5,8 @@ let instance = new p5(sketch);
 function reloadSketch() {
   if (!instance) return;
   instance.remove();
+  const svgCanvas = document.querySelector(".p5Canvas");
+  svgCanvas.remove();
   const seed = Math.floor(Math.random() * 1e9);
   instance = new p5((p) => sketch(p, seed));
 }
