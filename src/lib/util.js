a72e66ee3c4f0bc222d425624197cf8d02f53c17
Dylan Eck <akdylaneck@gmail.com>
2026-03-21 19:04:05 -0500

    fix async shader loading resolving to soon

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,6 +1,13 @@
-export async function loadShaderAsync(p5Inst, vertSrc, fragSrc) {
+export async function loadShaderAsync(p5Inst, vertPath, fragPath) {
   return new Promise((resolve, reject) => {
-    const shader = p5Inst.loadShader(vertSrc, fragSrc);
-    resolve(shader);
+    p5Inst.loadShader(
+      vertPath,
+      fragPath,
+      (shader) => resolve(shader),
+      (err) =>
+        reject(
+          err || new Error(`p5 failed to load shader ${vertPath}, ${fragPath}`),
+        ),
+    );
   });
 }
